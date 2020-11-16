e296104bada179674bfa318aa27829228c31a9e5
Muammarbanana <muammarfanani2@gmail.com>
2020-11-16 10:34:22 +0700

    add alarm receiver

---
+++ CodeLab4AlarmManager/app/src/main/java/com/example/myalarmmanager/AlarmReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.myalarmmanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val TYPE_ONE_TIME = "OneTimeAlarm"
+        const val TYPE_REPEATING = "RepeatingAlarm"
+        const val EXTRA_MESSAGE = "message"
+        const val EXTRA_TYPE = "type"
+        // Siapkan 2 id untuk 2 macam alarm, onetime dan repeating
+        private const val ID_ONETIME = 100
+        private const val ID_REPEATING = 101
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
